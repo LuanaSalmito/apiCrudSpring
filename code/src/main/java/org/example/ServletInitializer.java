644d8a54a468fae1b8afe6f22688aaddb9ca136e
Luana Salmito <119638705+LuanaSalmito@users.noreply.github.com>
2023-08-14 15:40:40 -0300

    falta configurar flyway

---
+++ code/src/main/java/org/example/ServletInitializer.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CrudApplication.class);
+    }
+}
